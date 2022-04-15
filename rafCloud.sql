0f4b7ee8771f8b901bc399125a7b710f943a8dd4
Skela97 <vlekovac@hotmail.com>
2022-04-15 16:12:11 +0200

    adding db

---
+++ rafCloud.sql
@@ -0,0 +1,51 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.5-10.4.24-MariaDB : Database - rafprojekat
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`rafprojekat` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `rafprojekat`;
+
+/*Table structure for table `machine` */
+
+DROP TABLE IF EXISTS `machine`;
+
+CREATE TABLE `machine` (
+  `idMachine` bigint(55) NOT NULL AUTO_INCREMENT,
+  `status` varchar(55) DEFAULT NULL,
+  `createdBy` bigint(20) DEFAULT NULL,
+  `dateFrom` date DEFAULT NULL,
+  PRIMARY KEY (`idMachine`),
+  KEY `fk_user` (`createdBy`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`createdBy`) REFERENCES `user` (`userId`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `machine` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(55) DEFAULT NULL,
+  `password` varchar(55) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
