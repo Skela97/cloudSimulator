4089a7727eac8a50dcfc8688ddc74e33580f9ddc
Skela97 <vlekovac@hotmail.com>
2022-04-15 16:01:52 +0200

    first commit

---
+++ src/main/java/rs/edu/raf/cloudSimulationApi/CloudSimulationApiApplication.java
@@ -0,0 +1,13 @@
+package rs.edu.raf.cloudSimulationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSimulationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSimulationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/edu/raf/cloudSimulationApi/CloudSimulationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.edu.raf.cloudSimulationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSimulationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
